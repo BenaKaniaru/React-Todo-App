3bb0982a342bdf4f89bae42d7c83e0a6d593ba98
Benard Kaniaru <kaniarubenard623@gmailcom>
2024-11-26 13:25:18 +0300

    create header and style it using css modules

--- my-react-todo-app/src/App.jsx
+++ my-react-todo-app/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import TodoInput from "./components/TodoInput";
+import "./App.css";
+import Todo from "./components/Todo";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <TodoInput />
+      <Header />
+      <Todo />
     </>
   );
 }
--- my-react-todo-app/src/components/TodoItem.jsx
+++ my-react-todo-app/src/components/TodoItem.jsx
@@ -2,5 +2,9 @@ import React from "react";
 export default function TodoItem({ todo }) {
   //const { todo } = props;
   //receives todo data from the parent component (TodoItem) through props(destructured)
-  return <li>{todo}</li>;
+  return (
+    <div style={{ fontSize: "20px", color: "Blue", margin: "20px 30px" }}>
+      {todo}
+    </div>
+  );
 }
